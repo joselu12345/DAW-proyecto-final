93f52835cd273366aa831cbb2bbd347ac9d219e1
Jose Luis <joselu.ruzgil@gmail.com>
2025-11-20 14:49:30 +0100

    prueba

--- src/app/acerca/page.jsx
+++ src/app/acerca/page.jsx
@@ -1,13 +1,10 @@
-
-
 function Acerca() {
-    return (
-
-        <div>
-            <h1 className="text-3xl font-bold">Acerca de nosotros</h1>
-            <p>somos una nueva empresa de pizzas</p>
-        </div>
-    );
+  return (
+    <div>
+      <h1 className="text-3xl font-bold">Acerca de nosotros</h1>
+      <p>somos un Restaurante nuevo en la ciudad</p>
+    </div>
+  );
 }
 
 export default Acerca;
--- src/components/header.js
+++ src/components/header.js
@@ -1,34 +1,38 @@
-import Link from 'next/link'
-import { auth } from "@/auth"
-import { logout } from '@/lib/actions'
-import { Pizza } from 'lucide-react';
-
+import Link from "next/link";
+import { auth } from "@/auth";
+import { logout } from "@/lib/actions";
+import { Pizza } from "lucide-react";
 
 async function Header() {
-    const session = await auth();
-    // console.log(session);
+  const session = await auth();
+  // console.log(session);
 
-    return (
-        <header className='bg-amber-600 text-white flex px-10 py-2 justify-between items-center'>
-            <nav className='flex gap-4 items-center'>
-                
-                <Link className='flex gap-3 items-center' href="/">
-                    <Pizza /> <h1 className="text-2xl font-bold hidden md:block">Joselu's pizza</h1>
-                </Link>
-                {/* {session?.user?.role === 'ADMIN'
+  return (
+    <header className="bg-amber-600 text-white flex px-10 py-2 justify-between items-center">
+      <nav className="flex gap-4 items-center">
+        <Link className="flex gap-3 items-center" href="/">
+          <Pizza />{" "}
+          <h1 className="text-2xl font-bold hidden md:block">
+            Restaurante Joselu App
+          </h1>
+        </Link>
+        {/* {session?.user?.role === 'ADMIN'
                     && <Link href="/admin">Admin panel</Link>
                 } */}
-                { session &&  <Link href="/dashboard">Dashboard</Link>}
-                <Link href="/acerca">Acerca</Link>
-            </nav>
-            <div className='flex gap-4'>
-                {session
-                    ? <form><button formAction={logout}>Logout</button></form>
-                    : <Link href="/auth/login">Login</Link>
-                }
-            </div>
-        </header>
-    )
+        {session && <Link href="/dashboard">Dashboard</Link>}
+        <Link href="/acerca">Acerca</Link>
+      </nav>
+      <div className="flex gap-4">
+        {session ? (
+          <form>
+            <button formAction={logout}>Logout</button>
+          </form>
+        ) : (
+          <Link href="/auth/login">Login</Link>
+        )}
+      </div>
+    </header>
+  );
 }
 
-export default Header
+export default Header;
