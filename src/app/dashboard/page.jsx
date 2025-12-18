cfbdaf3bcbe4b0079063cfc565e091b4510f4272
Jose Luis <joselu.ruzgil@gmail.com>
2025-12-18 06:07:36 +0100

    ultima

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,24 +1,18 @@
 import { auth } from "@/auth";
 import { redirect } from "next/navigation";
 
-
 async function Dashboard() {
+  const sesion = await auth();
+  if (!sesion) redirect("/auth/login");
 
-    const sesion = await auth()
-    if (!sesion) redirect ('/auth/login')
-
-    return ( 
-
-        <div>
-            <h1 className="text-3xl font-bold">
-                DASHBOARD
-            </h1>
-            <p>{sesion.user.name}</p>
-            <p>{sesion.user.email}</p>
-            <img src={sesion.user.image}/>
-        </div>
-
-     );
+  return (
+    <div className="p-10 m-10">
+      <h1 className="text-3xl font-bold">DASHBOARD</h1>
+      <p>Nombre de usuario: {sesion.user.name}</p>
+      <p>Correo: {sesion.user.email}</p>
+      <img src={sesion.user.image} />
+    </div>
+  );
 }
 
 export default Dashboard;
