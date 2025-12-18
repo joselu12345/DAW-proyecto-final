7de74e561904b2673efc2ef55559ded36b942035
Jose Luis <joselu.ruzgil@gmail.com>
2025-12-18 08:21:03 +0100

    vercel4

--- next.config.mjs
+++ next.config.mjs
@@ -20,6 +20,14 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+      },
+    ],
+  },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
