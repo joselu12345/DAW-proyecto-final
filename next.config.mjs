5090cdea6e49839307003c14258e0e2a13a52713
Jose Luis <joselu.ruzgil@gmail.com>
2025-12-18 07:50:44 +0100

    vercel2

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,25 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+
+//     images: {
+//         remotePatterns:[
+//             {
+//                 protocol: "https",
+//                 hostname: "res.cloudinary.com"
+
+//             }
+//         ]
+//     }
+
+// };
+
+// export default nextConfig;
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    
-    images: {
-        remotePatterns:[
-            {
-                protocol: "https",
-                hostname: "res.cloudinary.com"
-                
-            }
-        ]
-    }
-
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
